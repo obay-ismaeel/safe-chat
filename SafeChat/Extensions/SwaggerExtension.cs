2d2f5df1ea4419de1bfab501b77bfecc5f366343
Eyas Wannous <eyaswannous@gmail.com>
2024-11-22 19:21:38 +0300

    Add Extensions for 'program.cs'

---
+++ SafeChat/Extensions/SwaggerExtension.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace SafeChat.Extensions;
+
+public static class SwaggerExtension
+{
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(option =>
+        {
+            option.SwaggerDoc("v1", new OpenApiInfo { Title = "BudgetBlitz API", Version = "v1" });
+
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        return services;
+    }
+}
