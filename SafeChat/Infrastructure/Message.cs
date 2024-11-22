6508181de9b5a211ba5962166a715dd195f9cd41
Eyas Wannous <eyaswannous@gmail.com>
2024-11-22 23:08:59 +0300

    Update Message

--- SafeChat/Infrastructure/Message.cs
+++ SafeChat/Infrastructure/Message.cs
@@ -2,6 +2,7 @@
 
 public class Message
 {
+    public Guid Id { get; set; }
     public Guid SenderId { get; set; }
     public Guid ReceiverId { get; set; }
     public string Content { get; set; } = string.Empty;
