56687771150af019ef7f9d99a97b679cce426fb4
Eyas Wannous <eyaswannous@gmail.com>
2024-11-22 19:22:55 +0300

    Add UserAppService

---
+++ SafeChat/Application/Users/IUserAppService.cs
@@ -0,0 +1,8 @@
+﻿using SafeChat.Domain.Users;
+
+namespace SafeChat.Application.Users;
+
+public interface IUserAppService
+{
+    Task<(string Message, bool IsSuccess, User? User)> LoginUserUsingUserNameAsync(string userName, string password);
+}
---
+++ SafeChat/Application/Users/UserAppService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using SafeChat.Domain.Users;
+
+namespace SafeChat.Application.Users;
+
+public class UserAppService : IUserAppService
+{
+    private readonly UserManager<User> _userManager;
+
+    public UserAppService(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<(string Message, bool IsSuccess, User? User)> LoginUserUsingUserNameAsync(string userName, string password)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+        if (user is null)
+            return (Message: "There is a user with that UserName.", IsSuccess: false, User: null);
+
+        var result = await _userManager.CheckPasswordAsync(user, password);
+        if (!result)
+            return (Message: "UserName or Password is not correct.", IsSuccess: false, User: null);
+
+        return (Message: "Logged in Successfully!", IsSuccess: true, User: user);
+    }
+}
