ea7d96ec3e5e65e365cc03a0880c1e7ee7703aae
Eyas Wannous <eyaswannous@gmail.com>
2024-11-22 19:22:47 +0300

    Add Some DTOs

---
+++ SafeChat/Application/Users/DTOs/IncomingUserLoginUsingUserNameDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SafeChat.Application.Users.DTOs;
+
+public class IncomingUserLoginUsingUserNameDTO
+{
+    [Required]
+    [StringLength(50)]
+    public string UserName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50, MinimumLength = 5)]
+    public string Password { get; set; } = string.Empty;
+}
