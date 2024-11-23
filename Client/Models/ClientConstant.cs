9807fa700ef95f8ae4b4d00b65f63b4322323c50
Eyas Wannous <eyaswannous@gmail.com>
2024-11-24 01:36:03 +0300

    Update Constant

--- Client/Models/ClientConstant.cs
+++ Client/Models/ClientConstant.cs
@@ -1,10 +1,13 @@
 ﻿namespace Client.Models;
 
-public static class Constant
+public static class ClientConstant
 {
     public const string BaseUrl = "https://localhost:7239/";
     public const string BaseUrlApi = "https://localhost:7239/api/";
     public const string LoginApi = $"{BaseUrlApi}Auth/login";
     public const string UserApi = $"{BaseUrlApi}Chat/users";
     public const string SignalRChat = "https://localhost:7239/chat-hub";
+
+    public static Dictionary<string, string> AESKeys = [];
+    public static Dictionary<string, (string PublicKey, string PrivateKey)> AsymmetricKeys = [];
 }
